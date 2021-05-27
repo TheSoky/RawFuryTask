3f3957ed1272e13bc3e832ee7660309b471a6e65
Martin Šoštar <martin_sostar@yahoo.de>
2021-05-27 20:03:30 +0200

    Task 2: Maze escape pathfinder (with borrowed maze generator more in readme)

---
+++ Assets/MyAssets/Scripts/Maze/MazeGeneration/CellScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CellScript : MonoBehaviour
+{
+    //Serialized fields
+    [SerializeField]
+    private GameObject _leftWall;
+
+    [SerializeField]
+    private GameObject _rightWall;
+
+    [SerializeField]
+    private GameObject _upWall;
+
+    [SerializeField]
+    private GameObject _downWall;
+
+    //Private fields
+    private SpriteRenderer _spriteRenderer;
+
+    private bool _isGoalCell = false;
+
+    private List<CellScript> _neighbourCells = new List<CellScript>();
+
+    private bool _isVisitedByPathfinding = false;
+
+    //Properties
+    public GameObject LeftWall { get { return _leftWall; } }
+    public GameObject RightWall { get { return _rightWall; } }
+    public GameObject UpWall { get { return _upWall; } }
+    public GameObject DownWall { get { return _downWall; } }
+    public bool IsGoalCell { get { return _isGoalCell; } set { _isGoalCell = value; } }
+    public List<CellScript> NeighbourCells { get { return _neighbourCells; } set { _neighbourCells = value; } }
+
+    public bool IsVisitedByPathfinding { get { return _isVisitedByPathfinding; } set { _isVisitedByPathfinding = value; } }
+
+
+
+    //UnityCallbacks
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        if(_spriteRenderer == null)
+        {
+            Debug.LogError("Cells are missing SpriteRenderer!");
+        }
+    }
+
+    //Public methods
+    public void SetCellColor(Color newColor)
+    {
+        if(_spriteRenderer != null)
+        {
+            _spriteRenderer.color = newColor;
+        }
+    }
+
+}
---
+++ Assets/MyAssets/Scripts/Maze/Pathfinding/PathfindingAI.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathfindingAI : MonoBehaviour
+{
+    //Helper Class
+    private class BranchData
+    {
+        public List<CellScript> CellData = new List<CellScript>();
+        public BranchData ParentBranch;
+        public List<BranchData> ChildrenBranches = new List<BranchData>();
+        public bool IsEveryChildDeadEnd = false;
+    }
+
+
+    [SerializeField]
+    private MazeGenerator _mazeGenerator;
+
+    [Header("Maze solving color settings")]
+
+    [SerializeField]
+    private Color _currentPoints = Color.yellow;
+
+    [SerializeField]
+    private Color _activeBranches = Color.blue;
+
+    [SerializeField]
+    private Color _deadEndColor = Color.red;
+
+    [SerializeField]
+    private Color _solutionColor = Color.green;
+
+    private List<BranchData> _currentBranches = new List<BranchData>();
+    private bool _isGoalFound = false;
+    private List<BranchData> _deadEndBranches = new List<BranchData>();
+
+    private void Start()
+    {
+        CellScript startingCell = _mazeGenerator.CentreCells[0].cScript;
+  
+        BranchData startingBranch = new BranchData();
+        startingBranch.CellData.Add(startingCell);
+        _currentBranches.Add(startingBranch);
+        startingCell.SetCellColor(_currentPoints);
+        StartCoroutine(ExploreNextBranches());
+    }
+
+    private IEnumerator ExploreNextBranches()
+    {
+
+        while(!_isGoalFound)
+        {
+            List<BranchData> branchesToAdd = new List<BranchData>();
+
+            foreach (BranchData currentBranch in _currentBranches)
+            {
+                List<CellScript> cellsToAdd = new List<CellScript>();
+
+                foreach (CellScript cell in currentBranch.CellData)
+                {
+                    if(!cell.IsVisitedByPathfinding)
+                    {
+                        cell.IsVisitedByPathfinding = true;
+                        cell.SetCellColor(_activeBranches);
+                        //found goal
+                        if (cell.IsGoalCell)
+                        {
+                            MarkBranchesAsGoal(_currentBranches);
+                        }
+                        //Dead end
+                        else if (cell.NeighbourCells.Count == 1)
+                        {
+                            MarkBranchAsDeadEnd(currentBranch);
+                        }
+                        //proceed on branch
+                        else if (cell.NeighbourCells.Count == 2)
+                        {
+                            //Handle starting case in which there are 2 unvisited cells in 2*2 starting grid
+                            if(!cell.NeighbourCells[0].IsVisitedByPathfinding && !cell.NeighbourCells[1].IsVisitedByPathfinding)
+                            {
+                                cellsToAdd.Add(cell.NeighbourCells[0]);
+                                cellsToAdd.Add(cell.NeighbourCells[1]);
+
+                                cell.NeighbourCells[0].SetCellColor(_currentPoints);
+                                cell.NeighbourCells[1].SetCellColor(_currentPoints);
+
+
+                            }
+                            //Handle other cases where it is proceeding through lines and corners without branching
+                            else
+                            {
+                                bool branchIsDeadEnd = true;
+                                foreach (CellScript neighbourCell in cell.NeighbourCells)
+                                {
+                                    if (!neighbourCell.IsVisitedByPathfinding)
+                                    {
+                                        branchIsDeadEnd = false;
+                                        cellsToAdd.Add(neighbourCell);
+                                        neighbourCell.SetCellColor(_currentPoints);
+
+                                    }
+                                }
+                                if (branchIsDeadEnd)
+                                {
+                                    MarkBranchAsDeadEnd(currentBranch);
+                                }
+                            }
+
+
+                        }
+                        //create new branches
+                        else if (cell.NeighbourCells.Count > 2)
+                        {
+                            bool branchIsDeadEnd = true;
+                            foreach (CellScript neighbour in cell.NeighbourCells)
+                            {
+                                if (!neighbour.IsVisitedByPathfinding)
+                                {
+                                    branchIsDeadEnd = false;
+                                    BranchData newBranch = new BranchData();
+                                    newBranch.CellData.Add(neighbour);
+                                    newBranch.ParentBranch = currentBranch;
+                                    branchesToAdd.Add(newBranch);
+                                    currentBranch.ChildrenBranches.Add(newBranch);
+                                    neighbour.SetCellColor(_currentPoints);
+                                }
+                            }
+                            if (branchIsDeadEnd)
+                            {
+                                MarkBranchAsDeadEnd(currentBranch);
+                            }
+                        }
+                    }
+                }
+                
+                foreach (CellScript cellToAdd in cellsToAdd)
+                {
+                    currentBranch.CellData.Add(cellToAdd);
+                }
+            }
+
+            foreach (BranchData branchToAdd in branchesToAdd)
+            {
+                _currentBranches.Add(branchToAdd);
+            }
+
+            RemoveDeadEndBranches();
+            yield return null;
+        }
+    }
+
+    private void MarkBranchAsDeadEnd(BranchData deadBranch)
+    {
+        deadBranch.IsEveryChildDeadEnd = true;
+        foreach (CellScript branchCell in deadBranch.CellData)
+        {
+            branchCell.SetCellColor(_deadEndColor);
+        }
+        _deadEndBranches.Add(deadBranch);
+
+        if (deadBranch.ParentBranch != null)
+        {
+            bool isEverySiblingDeadEnd = true;
+            foreach (BranchData siblingBranch in deadBranch.ParentBranch.ChildrenBranches)
+            {
+                if (!siblingBranch.IsEveryChildDeadEnd)
+                {
+                    isEverySiblingDeadEnd = false;
+                    break;
+                }
+
+            }
+            if(isEverySiblingDeadEnd)
+            {
+                MarkBranchAsDeadEnd(deadBranch.ParentBranch);
+            }
+        }
+    }
+
+    private void RemoveDeadEndBranches()
+    {
+        foreach (BranchData deadBranch in _deadEndBranches)
+        {
+            _currentBranches.Remove(deadBranch);
+        }
+        _deadEndBranches.Clear();
+    }
+
+    private void MarkBranchesAsGoal(List<BranchData> branchesToGoal)
+    {
+        _isGoalFound = true;
+        Debug.Log("Goal found!");
+
+        List<BranchData> goalBranch = new List<BranchData>();
+
+        for (int i = branchesToGoal.Count - 1; i > 0; i--)
+        {
+            foreach (CellScript finalCell in branchesToGoal[i].CellData)
+            {
+                if(finalCell.IsGoalCell)
+                {
+                    goalBranch.Add(branchesToGoal[i]);
+                    break;
+                }
+            }
+            if(goalBranch.Count>0)
+            { 
+                break; 
+            }
+        }
+
+        while(goalBranch[goalBranch.Count - 1].ParentBranch != null)
+        {
+            goalBranch.Add(goalBranch[goalBranch.Count - 1].ParentBranch);
+        }
+
+        foreach (BranchData branch in goalBranch)
+        {
+            foreach (CellScript goalCell in branch.CellData)
+            {
+                goalCell.SetCellColor(_solutionColor);
+            }
+        }
+
+    }
+
+}
